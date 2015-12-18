59ffae0f488860556e865b867be8734e653c079b
nacer <naasbel@gmail.com>
2015-12-18 15:52:47 +0100

    Re-engeenering project

---
+++ CandyCrush/src/fr/amu/candycrush/view/ViewMainFrame.java
@@ -0,0 +1,19 @@
+package fr.amu.candycrush.view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ViewMainFrame extends javax.swing.JFrame implements Observer {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
